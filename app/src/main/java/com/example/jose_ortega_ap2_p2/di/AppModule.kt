af6d7608caeda6bdec228d2e11f92e09579a5d14
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-07-20 11:12:17 -0400

    Creacion de AppModule.kt y primer metodo

---
+++ app/src/main/java/com/example/jose_ortega_ap2_p2/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.example.jose_ortega_ap2_p2.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+}
