e4a00098ad2d9b1cf64c35f4d5675fa4be40aa54
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-07-22 11:49:14 -0400

    Modificando el AppModule.kt para agregar 2 priemros metods

--- app/src/main/java/com/example/jose_ortega_ap2_p2/di/AppModule.kt
+++ app/src/main/java/com/example/jose_ortega_ap2_p2/di/AppModule.kt
@@ -1,22 +1,36 @@
 package com.example.jose_ortega_ap2_p2.di
 
+import com.example.jose_ortega_ap2_p2.data.remote.Api
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
+import kotlin.jvm.java
 
-@InstallIn(SingletonComponent::class)
 @Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
-    @Provides
+
     @Singleton
+    @Provides
     fun provideMoshi(): Moshi {
-        return Moshi
-            .Builder()
+        return Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApi(moshi: Moshi): Api {
+        return Retrofit.Builder()
+            .baseUrl("")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(Api::class.java)
     }
 }
