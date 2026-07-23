a6c7ed7462d2fa78ec649ec9e18d14cb7dacd663
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-07-22 20:01:40 -0400

    Creando AappModule

--- app/src/main/java/com/example/jose_ortega_ap2_p2/di/AppModule.kt
+++ app/src/main/java/com/example/jose_ortega_ap2_p2/di/AppModule.kt
@@ -1,23 +1,24 @@
 package com.example.jose_ortega_ap2_p2.di
 
-import com.example.jose_ortega_ap2_p2.data.remote.Api
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import com.example.jose_ortega_ap2_p2.data.remote.GastoApi
+import com.example.jose_ortega_ap2_p2.data.remote.remotedatasource.GastoRemoteDataSource
+import com.example.jose_ortega_ap2_p2.data.repository.GastoRepositoryImpl
+import com.example.jose_ortega_ap2_p2.domain.repository.GastoRepository
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
-import kotlin.jvm.java
 
-@Module
 @InstallIn(SingletonComponent::class)
+@Module
 object AppModule {
-
-    @Singleton
     @Provides
+    @Singleton
     fun provideMoshi(): Moshi {
         return Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
@@ -26,11 +27,17 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideApi(moshi: Moshi): Api {
+    fun provideApi(moshi: Moshi): GastoApi {
         return Retrofit.Builder()
-            .baseUrl("")
+            .baseUrl("https://api-2026-h7eddqgydxc0fmau.eastus2-01.azurewebsites.net/")
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
-            .create(Api::class.java)
+            .create(GastoApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGastoRepository(remoteDataSource: GastoRemoteDataSource): GastoRepository {
+        return GastoRepositoryImpl(remoteDataSource)
     }
 }
